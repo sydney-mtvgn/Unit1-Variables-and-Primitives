e2b77aec65540be4f5a4dbd11ebc52b095a2d863
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-09-08 11:55:25 -0400

    End of lesson 1

---
+++ Lesson1-Variables/variables.py
@@ -0,0 +1,28 @@
+print("Lesson1-Welcome to Variables!")
+# Demo1 - First Variable
+# print("========================================")
+print("=" * 30)
+name = "Elliot"
+age = 18
+print(name)
+print("Student Name:",name)
+print(f"Student Name: {name}")
+print(f"{name} is {age} years old.")
+
+print("=" * 30)
+
+#Demo2: Different Data Types
+studnet_name = "John" #string
+student_height = 5.8 #float
+is_absent = False #boolean
+
+print("=" * 30)
+#Demo3: Variables can change
+score = 85
+print(f"Score: {score}")
+score = 90
+print(f"Score: {score}")
+score = 100
+print(f"Score: {score}")
+score = True
+print(f"Score: {score}")
