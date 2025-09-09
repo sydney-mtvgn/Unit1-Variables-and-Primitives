0bc14b395f2559c5278cc0b157dd32326426cc50
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-09-08 21:39:20 -0400

    Lesson 1 variables practice hw

---
+++ Lesson1-Variables/practice.py
@@ -0,0 +1,10 @@
+print("Lesson1-Variables Practice")
+name = "Starman"
+print("Name:", name)
+age = 22
+print (f"{name} is {age} years old.")
+height = 5.9
+print ("Height:", height)
+print ("Superhero: True")
+mainpower = "controls time and space"
+print (f"Starman's main power is that he {mainpower}.")
