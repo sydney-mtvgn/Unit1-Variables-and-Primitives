73b7042fd44812767de5fc658d3a2bd05e9994b1
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-09-10 11:18:57 -0400

    add

---
+++ Lesson 3/l3_strings_data_starter_code.py
@@ -0,0 +1,15 @@
+# Lesson 3: String Basics - Simple Name Builder
+
+# Part 1: Creating string variables
+
+# Part 2: Combining strings
+
+# Part 3: String repetition
+
+# Part 4: String length
+
+# Part 5: Indexing and Slicing
+
+# Part 6: Creative combinations
+
+
